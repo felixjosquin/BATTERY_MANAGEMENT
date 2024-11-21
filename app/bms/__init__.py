07905e67ad7c1dcc404227697685802a32f2ffd4
felixjosquin <fj.josquin@gmail.com>
2024-11-21 19:49:48 +0100

    update logic

--- app/bms/__init__.py
+++ app/bms/__init__.py
@@ -1,6 +1,7 @@
 import logging
 from .bms_api import getAnologData
 from .serialManager import SerialManager
+from .bms_type import BMS_ANALOG_VALUE
 
 logger = logging.getLogger(__name__)
 
@@ -11,4 +12,4 @@ if logger.hasHandlers:
     logger.addHandler(console_handler)
     logger.setLevel(logging.DEBUG)
 
-__all__ = ["getAnologData", "SerialManager"]
+__all__ = ["getAnologData", "SerialManager", "BMS_ANALOG_VALUE"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Depends, FastAPI
+from sqlmodel import Session
 from app.bms import getAnologData, SerialManager
+from app.database import creat_bms_Record, get_db, create_db_and_tables
+
 
 SERIAL_PORT = "/dev/ttyUSB0"
 BAUD_RATE = 9600
@@ -11,6 +15,7 @@ serialManager = SerialManager(SERIAL_PORT, BAUD_RATE)
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     serialManager.open()
+    create_db_and_tables()
     yield
     serialManager.close()
 
@@ -18,7 +23,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-@app.get("/analog_value")
-def get_analog_value():
+@app.get("/")
+def get_analog_value(db: Annotated[Session, Depends(get_db)]):
     sucess, analogValue = getAnologData(serialManager)
+    creat_bms_Record(db, analogValue)
     return analogValue.__dict__
