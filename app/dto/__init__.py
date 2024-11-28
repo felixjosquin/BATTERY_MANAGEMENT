883d96e6ec4b20fc92796c1cf80c50b58badfb08
felixjosquin <fj.josquin@gmail.com>
2024-11-28 14:58:56 +0100

    add BMS_ESSENTIAL_RECORD

--- app/dto/__init__.py
+++ app/dto/__init__.py
@@ -1,3 +1,4 @@
 from .bms_complete_record import BMS_COMPLETE_RECORD
+from .bms_essential_record import BMS_ESSENTIAL_RECORD
 
-__all__ = ["BMS_COMPLETE_RECORD"]
+__all__ = ["BMS_COMPLETE_RECORD", "BMS_ESSENTIAL_RECORD"]
---
+++ app/dto/bms_essential_record.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class BMS_ESSENTIAL_RECORD(BaseModel):
+    soc: float
+    current: float
+    batt_volt: float
+    remain_cap: float
+    full_cap: float
+    env_temp: int
+    pack_temp: int
+    nb_cycle: int
+    soh: int
+    created_at: datetime
