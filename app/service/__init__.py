2ce9f46524a8f913241f081e173bb4e5ebcd3737
felixjosquin <fj.josquin@gmail.com>
2024-11-28 13:35:26 +0100

    add sevice module

---
+++ app/service/__init__.py
@@ -0,0 +1,4 @@
+from .custom_exceptions import CustomException
+from .analog_data import get_current_data
+
+__all__ = ["CustomException", "get_current_data"]
---
+++ app/service/analog_data.py
@@ -0,0 +1,13 @@
+from sqlmodel import Session
+from app.bms import get_analog_data
+from app.bms import SerialManager
+from app.database import creat_record
+from .custom_exceptions import CustomException
+
+
+def get_current_data(db: Session, ser: SerialManager):
+    sucess, analog_value = get_analog_data(ser)
+    if not sucess:
+        raise CustomException("Not successfully get analog data")
+    creat_record(db, analog_value)
+    return analog_value
